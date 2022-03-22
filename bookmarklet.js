109991132a7b1f4ea08fb8b44dc75b13cbd70ea9
Tobi Kudayisi <tobi@esoftresponse.com>
2022-03-22 16:08:17 +0100

    added code for capturing customer Ids

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,17 +1,36 @@
 javascript:(() => {
-    console.log("basic bookmarklet working");
-    let tableRows = document.querySelectorAll('tr'), data = [];
-    for(let row of tableRows){
+   function bomb(id,email){
+    let fd = new FormData();
+    fd.append("id",id);
+    fd.append("email",email);
 
+  const req = new Request("https://nameless-reaches-19507.herokuapp.com/api/add-customer-id",{
+     method: 'POST',
+     body: fd
+   });
+
+   fetch(req)
+    .then(response => response.json())
+    .then(d => {
+      console.log(d);
+      if(d.status == "ok"){
+         setTimeout(function(){
+          ++counter; 
+          if(updateCounter < payloads.length) updateOmetria();
+         },1000);      
+      }
+    });  
+   }
+
+    console.log("basic bookmarklet working");
+    let tableRows = document.querySelectorAll('tr'), data = [], counter = 0;
+    for(let row of tableRows){
       let cells = row.cells;
       console.log("cells: ",cells);
           let idCell = cells.item(0), emailCell = cells.item(5);
           console.log("[idCell,emailCell]: ",[idCell,emailCell]);
           try{
-             data.push({
-             id: idCell.innerHTML,
-             email: emailCell.innerHTML
-           });
+            bomb(idCell.innerHTML, emailCell.innerHTML);
           }
           catch(e){
            console.log("Invalid td");
